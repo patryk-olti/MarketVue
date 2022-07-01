4856029539329d23d2d481f02d60f98cc4eb62a5
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-07-01 15:42:45 +0200

    added some styles for newsList container

--- vue-project/src/news/NewsList.vue
+++ vue-project/src/news/NewsList.vue
@@ -1,7 +1,12 @@
 <template>
-    <div v-for="item in list" :key="item.id">
-        <NewsPost :title="item.title" :text="item.text" />
+    <div class='container'>
+        <div class='title'> Breaking News! </div>
+
+        <div v-for="item in list" :key="item.id" class='tile'>
+            <NewsPost :title="item.title" :text="item.text" />
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -18,22 +23,41 @@
                 list: [
                     {
                         id: 1,
-                        title: 'First news',
-                        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+                        title: 'Did Razzlekhan and Dutch Pull Off History’s Biggest Crypto Heist?',
+                        text: 'The hackers had been inside the Bitfinex servers for weeks before attempting the heist. They’d watched users on the cryptocurrency exchange buy and sell Bitcoins. They’d studied the commands that controlled the security system. It was as if they were hiding in an air duct above a bank’s vault, watching as tellers meticulously moved cash in and out, looking for vulnerabilities.'
                     },
                     {
                         id: 2,
-                        title: 'Second news',
-                        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+                        title: 'Can Crypto’s Richest Man Stand the Cold?',
+                        text: 'During the first few months of this year—back when buying digital tokens named after dog memes was still seen, at least in some of the most forward-thinking circles, as a perfectly reasonable way to participate in finance’s bright new future—the cryptocurrency exchange Binance promoted a new, low-risk way to get in on the action. It urged its customers to invest in something called TerraUSD. The token was what’s known in the trade as a “stablecoin,” a type of cryptocurrency that functions a bit like ...'
                     },
                     {
                         id: 3,
-                        title: 'Third news',
-                        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book'
+                        title: 'Elon Musk Sounds Off on Recession Risk, Twitter Deal and Trump',
+                        text: 'Elon Musk, the billionaire visionary and oftentimes erratic entrepreneur, struck a sober tone Tuesday as he forecast a probable recession in the US and left doubts about his commitment to a $44 billion takeover of Twitter Inc.'
                     }
                 ]
             }
         }
-
     }
 </script>
+
+<style scoped>
+    .container{
+        display: flex;
+        flex-direction: column;
+
+        padding: 1rem;
+    }
+    .title{
+        font-size: 2.4rem;
+        margin-bottom: 1rem;
+    }
+
+    .tile{
+        padding: 0.5rem;
+        border-bottom: 1px solid rgb(95, 95, 95);
+        box-shadow: 2px 2px 4px 0px rgb(86, 88, 102);
+        margin-bottom: 0.5rem;
+    }
+</style>
