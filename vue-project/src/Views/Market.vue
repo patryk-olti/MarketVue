f7f2b6a39865e502d9e5cbbd4ac51277a4ade097
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-07-04 17:10:05 +0200

    created container for currencies

--- vue-project/src/Views/Market.vue
+++ vue-project/src/Views/Market.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="container"> 
-        <p> Market </p>
-    </div>
+    <MarketList />
 </template>
 
 <script>
+import MarketList from '../market/MarketList.vue';
+
 export default{
     name: 'Market',
+    components: {
+        MarketList
+    }
 }
 </script>
---
+++ vue-project/src/market/MarketList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container"> 
+        <p> Market - Cup </p>
+        <nav> 
+            <div> Eth 2 </div>
+            <div> Eth </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'MarketList',
+    date(){
+        return{
+            ehernum: {
+                name: 'etherium',
+                shortcut: 'ETH',
+                course: [12,16,13,15]
+            }
+        }
+    }
+}
+</script>
