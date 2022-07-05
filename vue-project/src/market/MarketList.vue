0b10a5305fe356c1e2162719bb52005059dce90f
Patryk Oltuch <95oltuchpatryk@gmail.com>
2022-07-05 10:35:23 +0200

    created mechanism for generating random numbers for array

--- vue-project/src/market/MarketList.vue
+++ vue-project/src/market/MarketList.vue
@@ -1,24 +1,55 @@
 <template>
-    <div class="container"> 
-        <p> Market - Cup </p>
-        <nav> 
-            <div> Eth 2 </div>
-            <div> Eth </div>
-        </nav>
+    <div class="container">
+        <div class="content">
+            <p> Market - Cup </p>
+            <nav> 
+                <div> <span> {{ ethernum.name }} : </span> <span> {{ ehtCourse }} </span> </div>
+            </nav>
+        </div>
+
     </div>
 </template>
 
 <script>
 export default{
     name: 'MarketList',
-    date(){
+    data(){
         return{
-            ehernum: {
+            ethernum: {
                 name: 'etherium',
                 shortcut: 'ETH',
                 course: [12,16,13,15]
             }
         }
+    },
+    computed: {
+
+        ehtCourse(){
+            setInterval( () => {
+                let newValue = Math.floor(Math.random() * 10);
+                this.ethernum.course.push(newValue);
+            }, 10000 )
+            return this.ethernum.course;
+        }
+
     }
 }
 </script>
+
+<style scoped>
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 100vw;
+        min-height: 100vh;
+
+        border: 1px solid black;
+    }
+
+    .content{
+        border: 1px solid red;
+    }
+
+</style>
